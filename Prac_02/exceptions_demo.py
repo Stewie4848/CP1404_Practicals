6343ff656c7345d01230646ae081084795431408
Jack Stewart <jack.stewart1@my.jcu.edu.au>
2019-08-15 12:16:22 +1000

    Changed price range and increase

---
+++ Prac_02/exceptions_demo.py
@@ -0,0 +1,16 @@
+try:
+    numerator = int(input("Enter the numerator: "))
+    denominator = int(input("Enter the denominator: "))
+    fraction = numerator / denominator
+    print(fraction)
+except ValueError:
+    print("Numerator and denominator must be valid numbers!")
+except ZeroDivisionError:
+    print("Cannot divide by zero!")
+print("Finished.")
+
+"""
+1. ValueError will occur if you enter a letter
+2. ZeroDivisionError will occur if you divide by zero
+
+"""
