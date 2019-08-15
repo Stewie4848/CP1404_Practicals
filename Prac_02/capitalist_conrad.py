4da73ea3e7dcbe074d7d80c8c61adb5ecfc712f5
Jack Stewart <jack.stewart1@my.jcu.edu.au>
2019-08-15 12:05:29 +1000

    Changed price range and increase

--- Prac_02/capitalist_conrad.py
+++ Prac_02/capitalist_conrad.py
@@ -9,14 +9,15 @@ The price should be displayed to the nearest cent (e.g. $33.59, not $33.59182329
 """
 import random
 
-MAX_INCREASE = 0.1  # 10%
+MAX_INCREASE = 0.175  # 17.5%
 MAX_DECREASE = 0.05  # 5%
-MIN_PRICE = 0.01
-MAX_PRICE = 1000.0
+MIN_PRICE = 1.0
+MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
+day = 0
 
 price = INITIAL_PRICE
-print("${:,.2f}".format(price))
+print("Starting price: ${:,.2f}".format(price))
 
 while price >= MIN_PRICE and price <= MAX_PRICE:
     price_change = 0
@@ -30,6 +31,6 @@ while price >= MIN_PRICE and price <= MAX_PRICE:
         # generate a random floating-point number
         # between negative MAX_DECREASE and 0
         price_change = random.uniform(-MAX_DECREASE, 0)
-
+    day += 1
     price *= (1 + price_change)
-    print("${:,.2f}".format(price))
+    print("On day {} price is: ${:,.2f}".format(day, price))
