7427bb8a61dd4a0bf1062ae004b6bfaae08acd7e
Jack Stewart <jack.stewart1@my.jcu.edu.au>
2019-08-22 12:13:01 +1000

    Refactor password program to use functions

--- prac_03/Password_checker.py
+++ prac_03/Password_checker.py
@@ -1,9 +1,19 @@
-password_length = 5
-valid_password = False
-while valid_password is False:
+def main():
+    password_length = 5
+    valid_password = False
+    while valid_password is False:
+        password = get_password()
+        if len(password) >= password_length:
+            valid_password = True
+        else:
+            print("Invalid password")
+    print_asterisk(password)
+
+
+def print_asterisk(password):
+    print('*' * len(password))
+
+
+def get_password():
     password = input("Password: ")
-    if len(password) >= password_length:
-        valid_password = True
-    else:
-        print("Invalid password")
-print('*' * len(password))
+    return password
