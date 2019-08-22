3ec38013d017906a6d1cf2fff9fc949831121d72
Jack Stewart <jack.stewart1@my.jcu.edu.au>
2019-08-22 11:29:00 +1000

    Add Password Checker

---
+++ prac_03/Password_checker.py
@@ -0,0 +1,9 @@
+password_length = 5
+valid_password = False
+while valid_password is False:
+    password = input("Password: ")
+    if len(password) >= password_length:
+        valid_password = True
+    else:
+        print("Invalid password")
+print('*' * len(password))
