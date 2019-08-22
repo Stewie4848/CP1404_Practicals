d070e78f88088db29e49b287343ab025be1aabc5
Jack Stewart <jack.stewart1@my.jcu.edu.au>
2019-08-22 12:22:05 +1000

    Add get sales function

--- prac_03/sales_bonus.py
+++ prac_03/sales_bonus.py
@@ -1,12 +1,17 @@
 def main():
-    sales = float(input("Enter sales: $"))
+    sales = get_sales()
     while sales >= 0:
         if sales < 1000:
             sales_bonus = sales * 0.10
         else:
             sales_bonus = sales * 0.15
         print("$", sales_bonus)
-        sales = float(input("Enter sales: $"))
+        sales = get_sales()
+
+
+def get_sales():
+    sales = float(input("Enter sales: $"))
+    return sales
 
 
 main()
