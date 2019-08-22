fa333913ab57904d4fe0bc3544adc9faab70b4de
Jack Stewart <jack.stewart1@my.jcu.edu.au>
2019-08-22 12:19:23 +1000

    Add get number of items function

--- prac_03/shop_calculator.py
+++ prac_03/shop_calculator.py
@@ -1,5 +1,5 @@
 def main():
-    number_of_items = int(input("Number of items: "))
+    number_of_items = get_number_of_items()
     total_price = 0
     while number_of_items < 0:
         print("Invalid number of items")
@@ -10,6 +10,11 @@ def main():
     if total_price > 100:
         total_price *= 0.9
     print("Total price for {} items is ${:.2f}".format(number_of_items, total_price))
+
+
+def get_number_of_items():
+    number_of_items = int(input("Number of items: "))
+    return number_of_items
 
 
 main()
