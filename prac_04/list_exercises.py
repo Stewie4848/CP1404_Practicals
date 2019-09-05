d7209005b68aaf212be8f3d51d5cdf5200776633
Jack Stewart <jack.stewart1@my.jcu.edu.au>
2019-09-05 11:23:23 +1000

    Fix average number

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -7,13 +7,13 @@ def main():
     print("The last number is {}".format(numbers[-1]))
     print("The smallest number is {}".format(min(numbers)))
     print("The largest number is {}".format(max(numbers)))
-    print("The average number is {}".format(sum(numbers)))
+    print("The average number is {}".format(sum(numbers) / len(numbers)))
 
-    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
-                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
-                 'bob']
+    user_names = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                  'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+                  'bob']
     username = input("Username: ")
-    if username in usernames:
+    if username in user_names:
         print("Access granted")
     else:
         print("Access Denied")
