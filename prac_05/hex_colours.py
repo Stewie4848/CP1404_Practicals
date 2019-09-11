413e6ca0182a54b63f63d2673d8faf08ae31d183
Stewie <jack.stewart17@yahoo.com.au>
2019-09-11 13:20:25 +1000

    Add hex colour dict

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,3 @@
+HEX_COLOURS = {"aliceblue": "#f0f8ff", "azure1": "#f0ffff", "aquamarine1": "#7fffd4", "beige": "#ffe4c4",
+               "blueviolet": "#8a2be2", "burlywood": "#deb887", "cadetblue": "#5f9ea0", "darkgreen": "#006400",
+               "floralwhite": "#fffaf0", "ghostwhite": "#f8f8ff", "gainsboro": "#dcdcdc"}
