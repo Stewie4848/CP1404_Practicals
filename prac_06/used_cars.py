c5228f148fffa39b58bb450267cccc5bb4fcd0c4
Jack Stewart <jack.stewart1@my.jcu.edu.au>
2019-09-12 11:51:48 +1000

    Finish car and used cars

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -6,7 +6,7 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car("David", 180)
     my_car.drive(30)
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
@@ -15,11 +15,12 @@ def main():
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=my_car))
 
-    limo = Car(100)
+    limo = Car("Geoff", 100)
     limo.add_fuel(20)
     print("Limo: ", limo.fuel)
     limo.drive(115)
     print("Limo odometer: ", limo.odometer)
+    print(limo)
 
 
 main()
