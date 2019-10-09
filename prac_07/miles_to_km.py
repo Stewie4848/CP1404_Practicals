e7529dc61646a76dabae6e00b8b7641948cba695
Stewie <jack.stewart17@yahoo.com.au>
2019-10-09 23:00:28 +1000

    Finish miles to km exercise

--- prac_07/miles_to_km.py
+++ prac_07/miles_to_km.py
@@ -15,15 +15,27 @@ class DistanceConverter(App):
         self.root = Builder.load_file("miles_to_km.kv")
         return self.root
 
-    def handle_convert(self, value):
+    # def handle_convert(self, value):
+    #     value = self.convert_to_number(value)
+    #     kilometres = value * MILES_TO_KM
+    #     self.root.ids.output_label.text = str(kilometres)
+
+    def handle_calculate(self, value):
         value = self.convert_to_number(value)
-        kilometres = value * MILES_TO_KM
-        self.root.ids.output_label.text = str(kilometres)
+        self.update_message(value)
+
+    def handle_increment(self, text, change):
+        miles = self.convert_to_number(text) + change
+        self.root.ids.input_number.text = str(miles)
+
+    def update_message(self, miles):
+        self.message = str(miles * MILES_TO_KM)
 
     @staticmethod
     def convert_to_number(value):
         try:
             value = float(value)
+            print("Converted")  # This runs twice if incrementing an existing number
             return value
         except ValueError:
             return 0.0
